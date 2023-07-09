50d5a42fac8c60fb3a330270081f6adcaf0800ad
helloMicrobe <77686470+ALawaetz@users.noreply.github.com>
2023-07-09 01:30:03 +0100

    Update install_dependencies.py

--- install_dependencies.py
+++ install_dependencies.py
@@ -3,9 +3,9 @@ import os
 base_folder = os.getcwd()
 
 try:
-    os.mkdir(os.path.expanduser('~/opt/anaconda3/envs'))
+    os.mkdir(os.path.expanduser('~/anaconda3/envs'))
 except:
     pass
 
-os.chdir(os.path.expanduser('~/opt/anaconda3/envs'))
+os.chdir(os.path.expanduser('~/anaconda3/envs'))
 os.system('conda env create --file {}/pyrap_environment.yml --prefix pyRAP'.format(base_folder))
